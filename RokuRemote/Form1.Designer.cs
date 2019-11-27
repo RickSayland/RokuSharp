e95b6c6e4e010dba5ddc4914ba2168c5304774ee
rsayl <rsayl@RICKDESKTOP>
2019-11-27 17:16:10 -0500

    Added buttons, and class library for roku api

--- RokuRemote/Form1.Designer.cs
+++ RokuRemote/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RokuRemote
 {
-    partial class Form1
+    partial class RokuRemote
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnVolumeUp = new System.Windows.Forms.Button();
+            this.btnVolumeDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<---";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(93, 12);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(75, 23);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(12, 64);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "<---";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(93, 64);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = "--->";
+            this.btnRight.UseVisualStyleBackColor = true;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(51, 41);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(51, 93);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 5;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            // 
+            // btnVolumeUp
+            // 
+            this.btnVolumeUp.Location = new System.Drawing.Point(174, 12);
+            this.btnVolumeUp.Name = "btnVolumeUp";
+            this.btnVolumeUp.Size = new System.Drawing.Size(75, 48);
+            this.btnVolumeUp.TabIndex = 6;
+            this.btnVolumeUp.Text = "Volume Up";
+            this.btnVolumeUp.UseVisualStyleBackColor = true;
+            // 
+            // btnVolumeDown
+            // 
+            this.btnVolumeDown.Location = new System.Drawing.Point(174, 64);
+            this.btnVolumeDown.Name = "btnVolumeDown";
+            this.btnVolumeDown.Size = new System.Drawing.Size(75, 52);
+            this.btnVolumeDown.TabIndex = 7;
+            this.btnVolumeDown.Text = "Volume Down";
+            this.btnVolumeDown.UseVisualStyleBackColor = true;
+            // 
+            // RokuRemote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(269, 146);
+            this.Controls.Add(this.btnVolumeDown);
+            this.Controls.Add(this.btnVolumeUp);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.btnBack);
+            this.Name = "RokuRemote";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnVolumeUp;
+        private System.Windows.Forms.Button btnVolumeDown;
     }
 }
 
--- RokuRemote/Form1.cs
+++ RokuRemote/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RokuRemote
 {
-    public partial class Form1 : Form
+    public partial class RokuRemote : Form
     {
-        public Form1()
+        public RokuRemote()
         {
             InitializeComponent();
         }
